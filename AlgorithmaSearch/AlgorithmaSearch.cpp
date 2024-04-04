cad3c4920033f2ae0e5e036e91dd73eabc4ab8af
Irfansyah001 <syahirfan545@gmail.com>
2024-04-04 15:11:02 +0700

    membuat void input

--- AlgorithmaSearch/AlgorithmaSearch.cpp
+++ AlgorithmaSearch/AlgorithmaSearch.cpp
@@ -5,6 +5,11 @@ int arr[20]; // Array to be searched
 int n; // number of elements in the array
 int i; // index of array element
 
+void input()
+{
+
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
